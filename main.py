3df74b920a6d54f82c05e3e8b67f918de933018c
DeusExVerbum <mymorningpromenade@gmail.com>
2013-08-19 15:13:29 -0700

    Pass-your-own function is working (including all arguments).

--- main.py
+++ main.py
@@ -1,10 +1,24 @@
-import mpprogressbar
+import mpprogressbar, random, time
 import multiprocessing as mp
+
+def download(status, pname, *fargs):
+    count = random.randint(5, fargs[0][2])
+    for i in range(count):
+        status.put([pname, (i+1.0)/count])
+        time.sleep(0.1)
 
 def main():
     mppb = mpprogressbar.mpprogressbar()
     for i in range(mp.cpu_count()):
         mppb.append("Process %d" % (i+1))
-    mppb.start("Updating database", "All downloads finished!", 0.1)
+    mppb.start(
+        "Updating database",
+        "All downloads finished!",
+        0.1,
+        download,
+        10,
+        100,
+        1000
+        )
 
 main()
--- mpprogressbar.py
+++ mpprogressbar.py
@@ -6,33 +6,27 @@ import multiprocessing as mp
 class mpprogressbar(object):
     def __init__(self):
         self.pbar_names = []
+        self.status = mp.Queue()
 
     def append(self, pbar_name):
         self.pbar_names.append(pbar_name)
 
-    def start(self, header, end_msg, interval):
-        def download(status, filename):
-            count = random.randint(5, 100)
-            for i in range(count):
-                status.put([filename, (i+1.0)/count])
-                time.sleep(0.1)
+    def print_progress(self, header, progress, elapsed):
+        sys.stdout.write('\033[2J\033[H') #clear screen
+        combined_perc = 0
+        sys.stdout.write(header+"\n")
+        for pname, percent in progress.items():
+            bar = ('=' * int(percent * 20)).ljust(20)
+            percent = int(percent * 100)
+            combined_perc += percent
+            sys.stdout.write("%s [%s] %3s%%\n" % (pname, bar, percent))
+        sys.stdout.flush()
 
-        def print_progress(header, progress, elapsed):
-            sys.stdout.write('\033[2J\033[H') #clear screen
-            combined_perc = 0
-            sys.stdout.write(header+"\n")
-            for pname, percent in progress.items():
-                bar = ('=' * int(percent * 20)).ljust(20)
-                percent = int(percent * 100)
-                combined_perc += percent
-                sys.stdout.write("%s [%s] %3s%%\n" % (pname, bar, percent))
-            sys.stdout.flush()
-
-        status = mp.Queue()
+    def start(self, header, end_msg, interval, fxn, *fargs):
         progress = collections.OrderedDict()
         workers = []
         for pname in self.pbar_names:
-            child = mp.Process(target=download, args=(status, pname))
+            child = mp.Process(target=fxn, args=(self.status, pname, fargs,))
             child.start()
             workers.append(child)
             progress[pname] = 0.0
@@ -40,11 +34,9 @@ class mpprogressbar(object):
         stime = int(time.time())
         while any(i.is_alive() for i in workers):
             time.sleep(interval)
-            while not status.empty():
-                pname, percent = status.get()
+            while not self.status.empty():
+                pname, percent = self.status.get()
                 progress[pname] = percent
                 now = int(time.time())
-                print_progress(header, progress, (now - stime))
+                self.print_progress(header, progress, (now - stime))
         print end_msg
-
-
