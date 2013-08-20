a6f7c4a18b378bfa02ece9f973edb75192b4b136
DeusExVerbum <mymorningpromenade@gmail.com>
2013-08-19 21:25:57 -0700

    Multiprocessing progressbars working.

--- examples.py
+++ examples.py
@@ -1,24 +1,43 @@
-import mpprogressbar, random, time
+import random, time, pbfmp, sys
 import multiprocessing as mp
 
-def download(status, pname, *fargs):
-    count = random.randint(5, fargs[0][1])
-    for i in range(count):
-        status.put([pname, (i+1.0)/count])
-        time.sleep(0.1)
+def basic_function(header, pstatus, pid):
+    for i in range(100):
+        time.sleep(random.random())
+        pstatus[pid] += 1
 
-def main():
-    mppb = mpprogressbar.MPProgressbar()
-    for i in range(mp.cpu_count()):
-        mppb.append("Process %d" % (i+1))
-    mppb.start(
-        "Updating database",
-        "All downloads finished!",
-        0.1,
-        download,
-        10,
-        100,
-        1000
-        )
 
-main()
+def progbar_implement():
+    pb_update_interval = 1
+    end_msg =  'all downloads complete'
+    pname_prefix = "Process "
+    header = "Updating Database"
+    nprocs = mp.cpu_count()
+
+    pnames = []
+    #pstatus = collections.OrderedDict()
+    manager = mp.Manager()
+    pstatus = manager.dict()
+    procs = []
+    pbars = []
+    for i in range(nprocs):
+        pnames.append(pname_prefix + str(i+1))
+        pstatus[i] = 0
+        pbars.append(pbfmp.ProgressBar(widgets=[('%s' % pnames[i]), ' ', pbfmp.Percentage(), ' ', pbfmp.Bar('=', '[', ']'), ' ', pbfmp.ETA()], maxval=100).start())
+        child = mp.Process(target=basic_function, args=[header, pstatus, i])
+        procs.append(child)
+        child.start()
+
+
+    sys.stderr.write('\033[2J\033[H') #clear screen
+    print header
+    while any(i.is_alive() for i in procs):
+        time.sleep(pb_update_interval)
+        sys.stderr.write('\033[2J\033[H') #clear screen
+        print header
+        for i, pbar in enumerate(pbars):
+            pbar.update(pstatus[i])
+    print end_msg
+
+
+progbar_implement()
--- mpprogressbar.py
+++ mpprogressbar.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-import time, sys, collections, terminalsize
+import sys, collections, terminalsize, time
+import pbfmp
 import multiprocessing as mp
 
 class MPProgressbar(object):
@@ -16,30 +17,29 @@ class MPProgressbar(object):
         sys.stdout.write(header+"\n")
         xy = terminalsize.get_terminal_size()
         for pname, percent in progress.items():
-            #bar_width = xy[0] - len(pname) - 25
             bar_width = xy[0] - len(pname) - 8
             bar = ('=' * int(percent * bar_width)).ljust(bar_width)
             percent = int(percent * 100)
             sys.stdout.write("%s [%s] %3s%%\n" % (pname, bar, percent))
-            #sys.stdout.write("%s [%s] %3s%%   ETA: 00h00m00s\n" % (pname, bar, percent))
-            #sys.stdout.write("%s [%s] %3s%%  ETA: %3d\n" % (pname, bar, percent, eta))
         sys.stdout.flush()
 
     def start(self, header, end_msg, interval, fxn, *fargs):
         progress = collections.OrderedDict()
+        pbars = collections.OrderedDict()
         workers = []
         for pname in self.pbar_names:
             child = mp.Process(target=fxn, args=(self.status, pname, fargs,))
             child.start()
             workers.append(child)
             progress[pname] = 0.0
+            pbars[pname] = pbfmp.ProgressBar(widgets=[pbfmp.Percentage(), pbfmp.Bar()], maxval=300).start()
 
-        stime = int(time.time())
         while any(i.is_alive() for i in workers):
             time.sleep(interval)
             while not self.status.empty():
                 pname, percent = self.status.get()
                 progress[pname] = percent
-                now = int(time.time())
-                self.print_progress(header, progress, (now - stime))
+                time.sleep(0.01)
+                pbars[pname].update(i+1)
+                #self.print_progress(header, progress, (now - stime))
         print end_msg
